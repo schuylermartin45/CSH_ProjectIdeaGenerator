460f47b54f30e85633c6340598585863e48a49af
schuylermartin45 <schuylermartin45@gmail.com>
2014-03-08 18:50:48 -0500

    -Touching the idea copies it to clip board (no visual indication yet) -Using the "View Stack" button shows working stack of ideas (newest idea is on top) -Stack is limited to 50 entries at a time at most -Clicking on something in the stack puts it in the GUI

--- ProjectIdeaGenerator/app/src/main/java/com/projectideagenerator/app/FullscreenActivity.java
+++ ProjectIdeaGenerator/app/src/main/java/com/projectideagenerator/app/FullscreenActivity.java
@@ -4,13 +4,21 @@ import com.projectideagenerator.app.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.content.DialogInterface;
+
+import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -20,10 +28,16 @@ import android.widget.TextView;
  */
 public class FullscreenActivity extends Activity {
 
-    //Button object that stems from the GUI
+    //Button objects that stem from the GUI
     private Button generateBtn;
+    private Button stackBtn;
     //Output for the idea
     private TextView ideaDisp;
+    //Tracks phrases
+    //WAS actually a stack, but then I needed an array...
+    private ArrayList<String> theStack;
+    //Max size of the stack
+    private final int MAXSTACK = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +45,84 @@ public class FullscreenActivity extends Activity {
         // remove titlebar
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_fullscreen);
-
+        theStack = new ArrayList<String>();
 
         //This is how android links the XML to Java...COOL!
         generateBtn = (Button)findViewById(R.id.genBtn);
+        stackBtn = (Button)findViewById(R.id.stackBtn);
         ideaDisp = (TextView)findViewById(R.id.fullscreen_content);
+
+        //Clicking on an idea will copy it to the clip-board
+        ideaDisp.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                //prevent copying of default text...
+                if(theStack.size() != 0 )
+                {
+                    //Thank you IntelliJ for auto-completing camel-case vars
+                    ClipboardManager clipboardManager =
+                        (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+                    ClipData clipData = ClipData.newPlainText("CSHProjectText",ideaDisp.getText());
+                    clipboardManager.setPrimaryClip(clipData);
+                }
+            }
+        });
+
         //Generation button click-event control
         generateBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v)
             {
                 String idea = IdeaGenerator.generateIdea();
                 ideaDisp.setText(idea);
+                //idea is stored
+                theStack.add(idea);
+                //exceed the max, the remove from the end...
+                if(theStack.size() > MAXSTACK)
+                {
+                    theStack.remove(0);
+                }
             }
         });
 
+        //Draw the stack to the screen in a scroll-able list
+        stackBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                //Dialog to put ListView in
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(v.getContext());
+                alertBuilder.setTitle("Previous ideas:");
+                //control the list visuals
+                final String[] listToShow;
+                if(theStack.size() == 0)
+                {
+                    listToShow = new String[]{ "<You have no ideas>" };
+                }
+                else
+                {
+                    listToShow = theStack.toArray(new String[theStack.size()]);
+                    //b/c not actually a stack but I need to
+                    //display it as a true stack...so reverse it!
+                    for(int cntr=0; cntr<listToShow.length/2; cntr++)
+                    {
+                        String temp = listToShow[cntr];
+                        listToShow[cntr] = listToShow[listToShow.length - cntr - 1];
+                        listToShow[listToShow.length - cntr - 1] = temp;
+                    }
+                }
+                alertBuilder.setItems(listToShow, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        //Set the current text if non-zero
+                        if(theStack.size() != 0)
+                        {
+                            //clicking an item in the list will draw it back on the screen
+                            ideaDisp.setText(listToShow[item]);
+                        }
+                    }
+                });
+                AlertDialog alert = alertBuilder.create();
+                alert.show();
+            }
+        });
     }
 
     @Override
